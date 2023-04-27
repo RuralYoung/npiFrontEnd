26fa345a5f1e275594d3b8cf1da6904e4a76395d
RuralYoung <ruralyoungiii@gmail.com>
2023-04-27 14:31:23 -0400

    Was in over my head by compartmentalizing the code
    Will try and go back to clean it up and give each component their own file but for now, let's try to keep it simple.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-import SearchForm from './components/SearchForm';
 
 
 function App() {
+  const [backendData, setBackendData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await window.fetch("http://localhost:5000/");
+      const data = await response.json();
+      setBackendData(data);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <div className="content">
         <h1>NPI Registry Search</h1>
 
-        <SearchForm/>
+        <form>
+          <label>
+            City: <input type="text" name="myCity"/>
+          </label>
+          <div>
+            <button type="reset">Clear</button>
+            <button type="submit">Submit</button> 
+          </div>
+        </form>
+        
+        <div>
+          {backendData.result_count}
+        </div>
       </div>
     </div>
   );
--- src/components/HandleSubmit.js
+++
@@ -1,8 +0,0 @@
-export default function HandleSubmit(event) {
-    // Since we don't want to reload the page
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-
-    window.alert(`You have inputted in: ${formData.get("myCity")}`);
-}
