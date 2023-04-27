6c00bd562a7d8273e0f8063bc4c8e3a077806603
RuralYoung <ruralyoungiii@gmail.com>
2023-04-27 18:11:50 -0400

    The React page displays info on a table
    While the information being displayed is the static link provided in the GET node, it's one step closer to the form calling the get and then printing the result on the page.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <div className="content">
         <h1>NPI Registry Search</h1>
 
-        <form>
+        <form action>
           <label>
             City: <input type="text" name="myCity"/>
           </label>
@@ -29,8 +29,24 @@ function App() {
         </form>
         
         <div>
-          {backendData.result_count}
+          <h2>Results: {backendData.result_count}</h2>
         </div>
+
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>NPI</th>
+            <th>Primary Address</th>
+          </tr>
+          {/*We have to do the '?' as it asks if it exists first*/}
+          {backendData.results?.map((item, index) => (
+            <tr key={index}>
+              <td>{item.basic.organization_name}</td>
+              <td>{item.number}</td>
+              <td>{item.addresses[1].address_1} {item.addresses[1].city}, {item.addresses[1].state} {item.addresses[1].postal_code.slice(0,5)}-{item.addresses[1].postal_code.slice(5,9)} {item.addresses[1].country_name}</td>
+            </tr>
+          ))}
+          </table>
       </div>
     </div>
   );
